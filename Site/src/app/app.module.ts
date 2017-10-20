ac071e4ad61f4e786d85c4cfea303248ffbc6e53
Spectere99 <rflowers@saber98.com>
2017-10-19 20:48:36 -0400

    Added Material Angular to Site

--- Site/src/app/app.module.ts
+++ Site/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { DxDataGridModule } from 'devextreme-angular';
-
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {  MatSidenavModule,
+          MatToolbarModule,
+          MatButtonModule,
+          MatIconModule,
+          MatIconRegistry } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerComponent
-  ],
-  imports: [
-    BrowserModule,
-    DxDataGridModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+  BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [MatIconRegistry],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
