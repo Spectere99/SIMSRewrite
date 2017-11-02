78ad61a3d427820c4def266a66326cb23076c4ab
Spectere <rflowers@saber98.com>
2017-11-02 17:43:41 -0400

    Updated Site for Order List.

--- Site/src/app/order/order.component.ts
+++ Site/src/app/order/order.component.ts
@@ -7,68 +7,121 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
   dataSource: any;
+  order_statusSource: any;
+  order_typeSource: any;
+  user_Source: any;
 
   filterDate = new Date(2015, 1, 1);
 
     constructor() {
-      this.dataSource = {
-        store: {
-            type: 'odata',
-            url: 'http://localhost:56543/odata/orders'
-        },
-        select: [
-          'order_id',
-          'customer_id',
-          'order_number',
-          'order_type',
-          'purchase_order',
-          'order_date',
-          'order_due_date',
-          'order_status',
-          'taken_user_id',
-          'assigned_user_id',
-          'est_begin_date',
-          'act_begin_date',
-          'est_complete_date',
-          'act_complete_date',
-          'shipped_date',
-          'subtotal',
-          'tax_rate',
-          'tax_amount',
-          'shipping',
-          'total',
-          'payments',
-          'balance_due',
-          'IMAGE_FILE',
-          'BILL_ADDRESS_1',
-          'BILL_ADDRESS_2',
-          'BILL_CITY',
-          'BILL_STATE',
-          'BILL_ZIP',
-          'SHIP_ADDRESS_1',
-          'SHIP_ADDRESS_2',
-          'SHIP_CITY',
-          'SHIP_STATE',
-          'SHIP_ZIP',
-          'PRIORITY',
-          'PERCENT_COMPLETE',
-          'ship_carrier',
-          'ship_tracking',
-          'previous_order',
-          'reorder_ind',
-          'ship_attn',
-          'contact',
-          'contact_email',
-          'contact_phone1',
-          'contact_phone1_ext',
-          'contact_phone1_type',
-          'contact_phone2',
-          'contact_phone2_ext',
-          'contact_phone2_type'
-        ],
-         filter: ['order_date', '>', this.filterDate]
-     };
+      this.createOrderDataSource();
+
+      this.order_statusSource = [
+      {char_mod: 'inq', description: 'Quote'},
+      {char_mod: 'inst', description: 'Invoiced'},
+      {char_mod: 'qtat', description: 'Quote/Artwork'},
+      {char_mod: 'ord', description: 'Order'},
+      {char_mod: 'com', description: 'Completed'},
+      {char_mod: 'clos', description: 'Closed'},
+      {char_mod: 'can', description: 'Cancelled'} ];
+     // this.createStatusDataSource();
+      this.createOrderTypeDataSource();
     }
+
+  createOrderDataSource() {
+    this.dataSource = {
+      store: {
+          type: 'odata',
+          url: 'http://localhost:56543/odata/orders'
+      },
+      select: [
+        'order_id',
+        'customer_id',
+        'reorder_ind',
+        'order_number',
+        'order_status',
+        'ship_attn',
+        'order_date',
+        'order_type',
+        'purchase_order',
+        'order_due_date',
+        'assigned_user_id',
+        'taken_user_id',
+        'est_begin_date',
+        'act_begin_date',
+        'est_complete_date',
+        'act_complete_date',
+        'shipped_date',
+        'subtotal',
+        'tax_rate',
+        'tax_amount',
+        'shipping',
+        'total',
+        'payments',
+        'balance_due',
+        'IMAGE_FILE',
+        'BILL_ADDRESS_1',
+        'BILL_ADDRESS_2',
+        'BILL_CITY',
+        'BILL_STATE',
+        'BILL_ZIP',
+        'SHIP_ADDRESS_1',
+        'SHIP_ADDRESS_2',
+        'SHIP_CITY',
+        'SHIP_STATE',
+        'SHIP_ZIP',
+        'PRIORITY',
+        'PERCENT_COMPLETE',
+        'ship_carrier',
+        'ship_tracking',
+        'previous_order',
+        'contact',
+        'contact_email',
+        'contact_phone1',
+        'contact_phone1_ext',
+        'contact_phone1_type',
+        'contact_phone2',
+        'contact_phone2_ext',
+        'contact_phone2_type'
+      ],
+       filter: ['order_date', '>', this.filterDate]
+   };
+  }
+
+  createStatusDataSource() {
+    this.order_statusSource = {
+      store: {
+          type: 'odata',
+          url: 'http://localhost:56543/odata/LookupItems'
+      },
+      select: [
+        'id',
+        'class',
+        'description',
+        'char_mod'
+      ],
+      filter: ['class', 'eq', 'ord']
+    };
+  }
+
+  createOrderTypeDataSource() {
+    this.order_typeSource = {
+      store: {
+          type: 'odata',
+          url: 'http://localhost:56543/odata/LookupItems'
+      },
+      select: [
+        'id',
+        'class',
+        'description',
+        'char_mod'
+      ],
+      filter: ['class', 'eq', 'otyps']
+    };
+  }
+  convertToBooleanDisplay(value) {
+    return value === 'Y';
+  }
   ngOnInit() {
   }
 
