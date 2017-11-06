53fe22fc33b3297f267f9cace827ee79193d1c8c
Spectere99 <rflowers@saber98.com>
2017-11-06 17:31:15 -0500

    Updated Customer Site Page

--- Site/src/app/customer/customer.component.ts
+++ Site/src/app/customer/customer.component.ts
@@ -6,9 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+dataSource: any;
 
-  constructor() { }
+  constructor() {
+    this.CreateCustomerDataSource();
+   }
 
+  CreateCustomerDataSource() {
+    this.dataSource = {
+      store: {
+          type: 'odata',
+          url: 'http://localhost:56543/odata/customers'
+      },
+      //expand: ['customer_person'],
+      select: [
+        'customer_id',
+        'customer_name',
+        'setup_date',
+        'setup_by',
+        'status_code',
+        'ship_to_bill_ind',
+        'website_url',
+        'account_number',
+        'override_validation_ind',
+        'parent_id',
+        'parent_ind'
+      ],
+       //filter: ['order_date', '>', this.filterDate]
+   };
+  }
   ngOnInit() {
   }
 
