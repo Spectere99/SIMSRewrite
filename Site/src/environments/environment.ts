e9c5b73ddec38974d11c8d0f3d6974f8e00974e4
Spectere99 <rflowers@saber98.com>
2018-03-16 08:50:39 -0400

    Updated configuraion for QA Build

--- Site/src/environments/environment.ts
+++ Site/src/environments/environment.ts
@@ -5,10 +5,10 @@
 
 export const environment = {
   production: false,
-  authEndpoint: 'http://localhost:56543/api/Security',
-  // authEndpoint: 'http://localhost:8888/api/Security',
-  // odataEndpoint: 'http://localhost:8888/odata/',
-  odataEndpoint: 'http://localhost:56543/odata/',
+  // authEndpoint: 'http://localhost:56543/api/Security',
+  authEndpoint: 'http://localhost:8888/api/Security',
+  odataEndpoint: 'http://localhost:8888/odata/',
+  // odataEndpoint: 'http://localhost:56543/odata/',
   artUploadURL: 'http://localhost:8888/api/ArtFile',  // DO NOT USE Slash at end!
   // artUploadURL: 'http://localhost:56543/api/ArtFile',
   defaultArtFolder: 'http://localhost:8888/orderimage/'
